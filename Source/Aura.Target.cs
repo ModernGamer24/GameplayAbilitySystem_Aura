7af7fef3f23ee890a32b6b6656ba1193b52d47c2
Egor Sivenkov <sivenkov846@mail.ru>
2024-02-28 17:42:30 +0700

    Character Creation

---
+++ Source/Aura.Target.cs
@@ -0,0 +1,15 @@
+// Aura learning project. xModern 2024
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class AuraTarget : TargetRules
+{
+	public AuraTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V4;
+
+		ExtraModuleNames.AddRange( new string[] { "Aura" } );
+	}
+}
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Aura learning project. xModern 2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+
